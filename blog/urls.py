04d1b3dee3b03c82a25028cd9c5a7bf14c1c2500
AmanPatwa <amanpatwa999@gmail.com>
2020-03-09 14:56:41 +0530

    User and blog created

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views as blog_views
+
+urlpatterns = [
+    path('',blog_views.home,name = 'blog-home'),
+    path('about/',blog_views.about,name = 'blog-about'),
+]
