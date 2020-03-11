a6c0b954d2e18693528f476631b4069a19b7a827
AmanPatwa <amanpatwa999@gmail.com>
2020-03-12 00:06:00 +0530

    pagination done

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views as blog_views
-from .views import HomeListView,PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import HomeListView,PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('',HomeListView.as_view(),name = 'blog-home'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('post/new/',PostCreateView.as_view(),name = 'post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name = 'post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name = 'post-delete'),
+    path('post/<str:username>',UserPostListView.as_view(),name = 'user-post'),
     path('script/', blog_views.script)
 ]
