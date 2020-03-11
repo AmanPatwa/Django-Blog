57d63458badd3b5d666b635b9d6a7a2a14d58d8b
AmanPatwa <amanpatwa999@gmail.com>
2020-03-11 20:26:17 +0530

    crud operation on post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views as blog_views
+from .views import HomeListView,PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('',blog_views.home,name = 'blog-home'),
+    path('',HomeListView.as_view(),name = 'blog-home'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
     path('about/',blog_views.about,name = 'blog-about'),
+    path('post/new/',PostCreateView.as_view(),name = 'post-create'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name = 'post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name = 'post-delete'),
+    path('script/', blog_views.script)
 ]
